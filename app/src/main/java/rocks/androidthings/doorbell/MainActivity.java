ee526daaf764f40ec34bf31a33093f28c66533ef
mplacona <marcos.placona@gmail.com>
2017-03-15 19:30:47 +0000

    First go at making it public

--- app/src/main/java/rocks/androidthings/doorbell/MainActivity.java
+++ app/src/main/java/rocks/androidthings/doorbell/MainActivity.java
@@ -17,44 +17,32 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * The GPIO pin to activate for button presses.
-     */
     private final String BUTTON_GPIO_PIN = "BCM6";
-
-    /**
-     * Driver for the doorbell button
-     */
-    private Button mButton;
-
-    /**
-     * Doorbell API
-     */
-    private Doorbell doorbellApi;
-
     private static final String TAG = "MainActivity";
+    private static final String BASE_URL = "http://06551871.ngrok.io";
 
-    private static final String BASE_URL = "http://6e9c6cf2.ngrok.io/";
+    private Button mButton;
+    private Doorbell mDoorbellApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initializeDoorbellButton();
+        initialiseDoorbellButton();
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .build();
 
-        doorbellApi = retrofit.create(Doorbell.class);
+        mDoorbellApi = retrofit.create(Doorbell.class);
     }
 
     /**
      * Initializes button driver, which will report physical button presses.
      */
-    private void initializeDoorbellButton() {
+    private void initialiseDoorbellButton() {
         try {
             mButton = new Button(BUTTON_GPIO_PIN,
                     Button.LogicState.PRESSED_WHEN_LOW);
@@ -68,23 +56,25 @@ public class MainActivity extends AppCompatActivity {
      * Callback for button events.
      */
     private Button.OnButtonEventListener mButtonCallback =
-            (button, pressed) -> {
-                if (pressed) {
-                    // Doorbell rang!
-                    Log.d(TAG, "button pressed");
-                    Call<String> call = doorbellApi.startCall();
-                    call.enqueue(new Callback<String>() {
-                        @Override
-                        public void onResponse(Call<String> call, Response<String> response) {
-                            Log.i(TAG, response.body());
-                        }
+            new Button.OnButtonEventListener() {
+                @Override
+                public void onButtonEvent(Button button, boolean pressed) {
+                    if (pressed) {
+                        // Doorbell rang!
+                        Log.d(TAG, "button pressed");
+                        Call<String> call = mDoorbellApi.startCall();
+                        call.enqueue(new Callback<String>() {
+                            @Override
+                            public void onResponse(Call<String> call, Response<String> response) {
+                                //Log.i(TAG, response.body());
+                            }
 
-                        @Override
-                        public void onFailure(Call<String> call, Throwable t) {
-                            Log.e(TAG, t.getMessage());
-                        }
-                    });
-
+                            @Override
+                            public void onFailure(Call<String> call, Throwable t) {
+                                Log.e(TAG, t.getMessage());
+                            }
+                        });
+                    }
                 }
             };
 
